678d1e915651e093cc5f420a87b626bb765a60bd
Weslleyw10 <weslleylopes.dev@gmail.com>
2021-10-14 21:38:23 -0300

    App finished

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { SigInButton } from '../SignInButton'
+import styles from './style.module.scss'
+import { ActiveLink } from '../ActiveLink'
+
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news" />
+                <nav>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a className={styles.active}>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+                        <a>Posts</a>
+                    </ActiveLink>
+                </nav>
+
+                <SigInButton />
+            </div>
+        </header>
+    )
+}
